3329afa5e36d7808cb6fabe1368000cc20171d2b
Jeremy Wells <jsheridanwells@gmail.com>
2019-04-24 21:16:12 -0500

    CRUD for category

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
-from resources.Category import Category
+from resources.Category import CategoryResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 api.add_resource(Hello, '/Hello')
-api.add_resource(Category, '/Category')
+api.add_resource(CategoryResource, '/Category')
